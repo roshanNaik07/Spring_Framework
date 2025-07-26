513649832d39e74fb2054b988c2a41466e50d673
roshanNaik07 <roshannaik202055>
2025-07-26 23:59:22 +0530

    Creating instances using spring framework

---
+++ Scott/src/main/java/com/xworkz/scott/fashion/Jack.java
@@ -0,0 +1,12 @@
+package com.xworkz.scott.fashion;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Jack {
+
+    public Jack(){
+        System.out.println("Jack instance created by Spring");
+
+    }
+}
